5b9b1f461bbd3129267a76dae5eea94ee05c61eb
PFISTER Ludovic <ludovicp2005@gmail.com>
2025-10-30 16:33:10 +0100

    catalogue

--- api/controllers/catalogue.controllers.js
+++ api/controllers/catalogue.controllers.js
@@ -3,6 +3,7 @@ exports.get = (req, res) => {
         const catalogue = [
 		{ref:"X001", titre : "Linux", prix : 10},
 		{ref:"X002", titre : "Angular", prix : 20}
+		{ref:"X003", titre : "Git", prix : 30}
 		];
 		
 	
